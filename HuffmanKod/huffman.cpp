c8837deb1ba3c8edbe32a582e7156ad63a257e97
almog zinger <almog389@gmail.com>
2023-01-15 16:42:26 +0200

    Add project files.

---
+++ HuffmanKod/huffman.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <queue>
+#include <list>
+#include <string>
+#include <cmath>
+#include "huffman.h"
+#include <unordered_map>
+using namespace std;
+
+int frequencyTable[26] = { 0 };
+
+string codedTable[26] = { 0 };
+
+void HuffmanTree::encodeLetters(HuffmanNode* root, string str)
+{
+	//if not a leaf 
+	if (root->left != nullptr)
+	{
+		encodeLetters(root->left, str += '0');
+		encodeLetters(root->right, str += '1');
+	}
+	//If a leaf
+	else
+	{
+		code
+	}
+}
+
+string HuffmanTree::encodeTree(HuffmanNode* root)
+{
+	string codeTree = "";
+	return encodeTree(root, codeTree);
+}
+string HuffmanTree::encodeTree(HuffmanNode * root, string code)
+{
+	if (root == nullptr)
+		return "";
+	//If there is spilt - 
+	if (root->left != nullptr)
+	{
+		return encodeTree(root->right, encodeTree(root->left, code += '0'));
+	}
+	else //If got to leaf
+		return "1";
+}
+	
+
+/// <summary>
+/// update the FrequencyTable
+/// </summary>
+/// <param name="text"></param>
+/// <returns>The amount of chater in the text</returns>
+int HuffmanTree::buildFrequencyTable(string text)
+{
+	int numOfCherInText = 0;
+	for (int i = 0; i < text.size(); i++)
+	{
+		//add 1 to coounter if this is the first time
+		if (frequencyTable[CHAR_TO_INDEX(text[i])] == 0) numOfCherInText++;
+		frequencyTable[CHAR_TO_INDEX(text[i])]++;
+	}
+	return numOfCherInText; 
+}
+HuffmanTree::HuffmanTree(string word)
+{
+	HuffmanNode* first;
+	string temp = "";
+	numOfCherInText = buildFrequencyTable(word);
+	for (int i = 0; i < 26; i++)
+	{
+		if (frequencyTable[i] > 0)
+		{
+			//Push to the queue 
+			temp.push_back('a' + i);
+			pQueue.push(new HuffmanNode(temp, frequencyTable[i]));
+		}
+	}
+	
+
+	//Build the tree
+	while (!pQueue.empty()) {
+		first = pQueue.top();
+		pQueue.pop();
+		if (!pQueue.empty())
+		{
+			HuffmanNode* secound = pQueue.top();
+			pQueue.pop();
+			pQueue.push(new HuffmanNode(first->str + secound->str, first->frequency + secound->frequency, first, secound));
+			secound = nullptr;
+		}
+	}
+
+	treeCode = encodeTree(first);
+	//prepare the code table
+
+	
+}
---
+++ HuffmanKod/huffman.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <iostream>
+#include <queue>
+#include <list>
+#include <string>
+#include <cmath>
+using namespace std;
+
+// Alphabet size (# of symbols) 
+#define ALPHABET_SIZE (26) 
+
+// Converts key current character into index 
+// use only 'a' through 'z' and lower case 
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a') 
+
+
+class compareNode;
+class HuffmanNode
+{
+public:
+	string str;
+	int frequency;
+	HuffmanNode* left;
+	HuffmanNode* right;
+
+	HuffmanNode(string str, int f) {
+		this->str = str;
+		this->frequency = f;
+		right = nullptr;
+		left = nullptr;
+	}
+
+	HuffmanNode(string str, int f, HuffmanNode* const& r, HuffmanNode* const& l) {
+		this->str = str;
+		this->frequency = f;
+		right = r;
+		left = l;
+	}
+	friend compareNode;
+};
+
+class compareNode 
+{
+public:
+	bool operator()(HuffmanNode* const& n1, HuffmanNode* const& n2)
+	{
+		return n1->frequency > n2->frequency;
+	}
+};
+
+class HuffmanTree
+{
+	priority_queue<HuffmanNode*, vector<HuffmanNode*>, compareNode> pQueue;
+	int numOfCherInText;
+	string treeCode;
+	HuffmanTree(string word);
+	string encodeTree(HuffmanNode* root);
+	string encodeTree(HuffmanNode* root,string);
+	void encodeLetters(HuffmanNode* root) { encodeLetters(root,""); }
+	void encodeLetters(HuffmanNode* root, string);
+	void decode(string sourceFileName, string destFileName);
+	int buildFrequencyTable(string text);
+	void buildTree(int* frequencyTable);
+	void buildTree(int n, string letters, string tree);
+	void buildCodedTabe();
+	void encode(char letter, string* codedTable);
+	~HuffmanTree();
+};
+
